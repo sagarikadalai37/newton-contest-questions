ca74b68f34696331cc9ecf746afc04c657b84c90
sagarikadalai37 <96329273+sagarikadalai37@users.noreply.github.com>
2023-01-22 18:30:56 +0530

    Create Print Pattern.java

---
+++ Print Pattern.java
@@ -0,0 +1,14 @@
+static void printPattern(int n,int curr, boolean flag)
+{
+   System.out.print(curr + " ");
+    if(flag == false && n == curr)
+    return;
+   if(flag){
+       if((curr-5) > 0 ) 
+      printPattern(n ,curr-5,true);
+       else 
+      printPattern(n , curr - 5,false);
+   }else {
+       printPattern(n ,curr + 5 , false );
+   } 
+}
