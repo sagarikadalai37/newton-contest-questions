5e6a4bba82b4d9e0dbb5958b142ddadf1088bd19
sagarikadalai37 <96329273+sagarikadalai37@users.noreply.github.com>
2023-01-22 18:42:14 +0530

    Create Minimum operation - II.java

---
+++ Minimum operation - II.java
@@ -0,0 +1,26 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+     Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int x = 1;
+        int y = 1;
+        int count  = 0;
+        while(x < n && y < n ){
+            if(x <= y){
+                x = x+y;
+            }else {
+                y = x+y;
+            }
+            count++;
+        }
+        if(n < 100000)
+        System.out.print(count);
+        else
+        System.out.print(count+1);
+    }
+}
